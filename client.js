25df7961a57560da3accc188ca6f80f0ceb244fc
Alastair Coote <alastair.coote@gmail.com>
2017-01-06 16:53:16 -0500

    don't serialize JSON

--- client.js
+++ client.js
@@ -2,28 +2,30 @@ module.exports = function (command, data) {
     
     // Wrap our postMessage in a promise to allow us to chain commands and
     // responses together.
-   
     return new Promise((fulfill, reject) => {
         let messageChannel = new MessageChannel();
         messageChannel.port1.onmessage = (event) => {
+            console.log(`Received response to ${command}...`)
             let [err, data] = event.data;
             if (err) {
                 return reject(err);
             }
             
+            // messageChannel.port1.close();
             fulfill(data);
         };
-        
-        let dataAsString = JSON.stringify(data);
         
         navigator.serviceWorker.ready.then((reg) => {
             console.info(`Sending ${command} to service worker...`, data);
             reg.active.postMessage({
                 action: "runCommand",
                 command,
-                dataAsString
+                data
             }, [messageChannel.port2]);
         })
         
     })
+}
+if (typeof window !== "undefined") {
+window.runServiceWorkerCommand = module.exports;
 }
--- service-worker.js
+++ service-worker.js
@@ -6,31 +6,31 @@ self.addEventListener('message', function(event){
         // allow other types of messages if needed
         return;
     }
+
     
     let ret = (err, data) => {
         event.ports[0].postMessage([err, data]);
     }
    
-    let {command, dataAsString} = event.data;
-    
-    let data = null;
-    if (dataAsString) {
-        data = JSON.parse(dataAsString);
-    }
-    
-    if (!boundFuncs[command]) {
-        console.warn(`Fired command ${command}, but no function was bound to it.`);
-        return;
-    }
-    
-    Promise.resolve(boundFuncs[command](data))
+    let {command, data} = event.data;
+
+    console.info(`Service worker received ${command}`)
+
+    module.exports.run(command, data)
     .then((data) => {
-        console.info(`Service worker replying to ${command}...`, data);
+        console.info(`Service worker replying to ${command}...`);
         event.ports[0].postMessage([null, data]);
     })
     .catch((err) => {
-        console.error(`Service worker replying with error to ${command}...`, err);
+        console.error(`Service worker replying with error to ${command}...`, err.message);
         event.ports[0].postMessage([err.toString(), null]);  
+    })
+    .then(() => {
+        try {
+            event.ports[0].close()
+        } catch (err) {
+            console.error("Could not close port")
+        }
     })
     
 });
@@ -42,10 +42,22 @@ module.exports = {
         }
         boundFuncs[command] = func;
     },
+    bindAll(obj) {
+        for (key in obj) {
+            module.exports.bind(key, obj[key]);
+        }
+    },
     unbind(command, func) {
         if (boundFuncs[command] !== func) {
             throw new Error(`Trying to unbind function from ${command} when it is not bound.`)
         }
         boundFuncs[command] = null;
+    },
+    run(command, data) {
+        if (!boundFuncs[command]) {
+            throw new Error("Tried to execute " + command + ", but it is not bound");
+        }
+
+        return Promise.resolve(boundFuncs[command](data))
     }
 };
